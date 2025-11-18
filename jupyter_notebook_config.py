406358bcdef4e9760bb2bc3aa8744024beedd280
vadimwinebrand <winebra@gmail.com>
2025-11-17 17:17:55 -0800

    Create jupyter_notebook_config.py

---
+++ jupyter_notebook_config.py
@@ -0,0 +1,4 @@
+
+c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$D3G8vU4foP1uJdWjlyUltg$CwPksoNUt8x64cubRW7vcAfkwlXd3ZksOnPfZHsdi9M'  # Replace with your hash
+c.NotebookApp.token = ''  # Disable token
+
